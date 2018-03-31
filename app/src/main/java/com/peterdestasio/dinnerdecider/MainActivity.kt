51896e49dd55301fcb53618e5618bad3a9420ad3
Pierluigi De Stasio <peterdestasio@gmail.com>
2018-03-30 20:12:13 -0400

    Added random generator of food and choices

--- app/src/main/java/com/peterdestasio/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/peterdestasio/dinnerdecider/MainActivity.kt
@@ -2,10 +2,13 @@ package com.peterdestasio.dinnerdecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "Munnezz")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         selectedFoodtxt.text = "Burg"
+
+        decideButton.setOnClickListener{
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodtxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener{
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+
+            addFoodText.text.clear()
+        }
     }
 }
